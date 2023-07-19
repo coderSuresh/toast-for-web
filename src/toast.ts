6eec4491b0ece71c9683d171b6cbb43ef083a895
codersuresh <sd807141@gmail.com>
2023-07-19 13:38:33 +0545

    feat: functional toast

--- src/toast.ts
+++ src/toast.ts
@@ -1,14 +1,31 @@
 const toastElem = document.createElement('p');
 toastElem.classList.add('web-toast');
 
+document.body.appendChild(toastElem);
+
+let timeOut: number;
+
+const removeToast = () => {
+    toastElem.classList.remove('show')
+}
+
+const removeElemOnClick = () => {
+    toastElem.addEventListener('click', () => {
+        removeToast();
+    })
+}
+
+removeElemOnClick();
+
 const showToast = (message: string, duration = 3000) => {
     toastElem.textContent = message;
     toastElem.classList.add('show');
-    document.body.appendChild(toastElem);
-    setTimeout(() => {
-        document.body.removeChild(toastElem);
-        toastElem.classList.remove('show');
-    }, duration);
+
+    if (timeOut) {
+        clearInterval(timeOut)
+    }
+
+   timeOut = setTimeout(removeToast, duration);
 }
-    
+
 export default showToast;
